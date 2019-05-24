cb951e881d75f3ef2a9373232e77f920ed516a2f
Ariana Gonzalez <arianagonzalez@Arianas-MacBook-Air.local>
2019-05-24 14:42:34 -0400

    pi.py

---
+++ pi.py
@@ -0,0 +1,34 @@
+from numpy import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 1000 #darts thrown
+
+circlex = []
+circley = []
+
+squarex = []
+squarey = []
+
+i = 1
+
+while i< N:
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    if (x**2 + y**2 <= 1):
+        circlex.append(x)
+        circley.append(y)
+    else:
+        squarex.append(x)
+        squarey.append(y)
+    i += 1
+    
+pi = 4*len(circlex)/float(N)
+    
+print("Pi is appoximately ", pi)
+
+print("Pi is actually ", np.pi)
+
+plt.plot(circlex,circley, 'b.')
+plt.plot(squarex,squarey,'g.')
+plt.savefig("Pi")
